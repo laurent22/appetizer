d057793977ebc8027c2382be583421d1c92e6194
Laurent <Laurent@ff61cf6c-2836-11df-8847-f3d5e5ef3dde>
2008-08-24 22:17:23 +0000

    Added StringConv and SplitString functions.
    git-svn-id: https://appetizer.googlecode.com/svn@44 ff61cf6c-2836-11df-8847-f3d5e5ef3dde

--- Perso/MiniLaunchBar/Développement/StringUtils.pas
+++ Perso/MiniLaunchBar/Développement/StringUtils.pas
@@ -2,11 +2,21 @@ unit StringUtils;
 
 interface
 
-uses Classes;
+uses Classes, SysUtils, Variants;
 
 function IsInStringList(const iStringList: TStringList; const iString: String):Boolean;
+function StringConv(const v:Variant):String;
+function SplitString(const Delimiter: Char; Input: string): TStringList;
 
 implementation
+
+
+function SplitString(const Delimiter: Char; Input: string): TStringList;
+begin
+	result := TStringList.Create();
+  result.Delimiter := Delimiter;
+	result.DelimitedText := Input;
+end;
 
 
 function IsInStringList(const iStringList: TStringList; const iString: String):Boolean;
@@ -19,6 +29,53 @@ begin
       break;
     end;
   end;
+end;
+
+
+function StringConv(const v:Variant):String;
+var t: Word;
+begin
+	t := VarType(v);
+
+  if (t = varNull) or (t = varEmpty) then begin
+  	result := 'nil';
+    Exit;
+  end;
+
+	if t = varBoolean then begin
+  	if Boolean(v) then
+    	result := 'true'
+    else
+    	result := 'false';
+    Exit;
+  end;
+
+  if t = varInteger then begin
+  	result := IntToStr(Integer(v));
+    Exit;
+  end;
+
+  if t = varWord then begin
+  	result := IntToStr(Word(v));
+    Exit;
+  end;
+
+  if t = varSingle then begin
+  	result := IntToStr(Integer(v));
+    Exit;
+  end;
+
+  if t = varByte then begin
+  	result := IntToStr(Byte(v));
+    Exit;
+  end;
+
+  if t = varString then begin
+  	result := String(v);
+    Exit;
+  end;
+
+  result := '<unknown type>';
 end;
 
 
