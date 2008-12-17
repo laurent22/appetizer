c376975f2938ae42656e237b66ba8607480da224
laurent1979 <laurent1979@ff61cf6c-2836-11df-8847-f3d5e5ef3dde>
2008-12-17 20:49:09 +0000

    - Added Japanese Help file
    git-svn-id: https://appetizer.googlecode.com/svn@263 ff61cf6c-2836-11df-8847-f3d5e5ef3dde

--- branches/version_1.2/_gettext.bat
+++ branches/version_1.2/_gettext.bat
@@ -9,8 +9,11 @@ msgmerge -U Data\Locales\nl\appetizer.po Data\Locales\appetizer.pot
 msgmerge -U Data\Locales\pl\appetizer.po Data\Locales\appetizer.pot
 msgmerge -U Data\Locales\es\appetizer.po Data\Locales\appetizer.pot
 msgmerge -U Data\Locales\da\appetizer.po Data\Locales\appetizer.pot
+msgmerge -U Data\Locales\ja\appetizer.po Data\Locales\appetizer.pot
+msgmerge -U Data\Locales\ko\appetizer.po Data\Locales\appetizer.pot
 
 xgettext --from-code=utf-8 -C -n -k_ -o Data\Help\appetizer_help.pot generate_help\main.cpp
 msgmerge -U Data\Help\fr\appetizer_help.po Data\help\appetizer_help.pot
 msgmerge -U Data\Help\en\appetizer_help.po Data\help\appetizer_help.pot
 msgmerge -U Data\Help\de\appetizer_help.po Data\help\appetizer_help.pot
+msgmerge -U Data\Help\ja\appetizer_help.po Data\help\appetizer_help.pot
